0fc462e9c382b51711f7f4f364169680f1f7ffa2
Atharva <atharvakhadake759@gmail.com>
2025-08-04 11:28:05 +0530

    Meta Info

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,17 +1,16 @@
 import { Link } from "react-router-dom";
-import { Menu } from "lucide-react";
 
 const Navbar = () => {
   return (
     <nav className="bg-[#0f172a] shadow-md">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 py-4 flex justify-between items-center">
         
-        {/* Logo / Brand */}
-        <Link to="/" className="text-2xl font-bold text-white tracking-wide">
-          EMS
+        <Link to="/" className=" flex justify-center items-center gap-3 text-2xl font-bold text-white tracking-wide">
+        <img className="w-15 h-15" src="/logo.svg" />
+          Employee Management System
         </Link>
 
-        {/* Navigation Links */}
+        
         <div className="hidden md:flex gap-8 items-center text-white text-sm font-medium">
           <Link to="/employees" className="hover:text-blue-300 transition">
             Employees
