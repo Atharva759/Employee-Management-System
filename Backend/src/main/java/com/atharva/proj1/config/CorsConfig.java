26523122da18c3edd608bca68a45338a7ff6b458
Atharva <atharvakhadake759@gmail.com>
2025-08-16 12:20:01 +0530

    cors fix

--- Backend/src/main/java/com/atharva/proj1/config/CorsConfig.java
+++ Backend/src/main/java/com/atharva/proj1/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://localhost:5173","https://employee-management-sys-afk.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
